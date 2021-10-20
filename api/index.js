4d5a69933d2a73a25c431072b5fd4cd5ef981d67
Tanveer Shah <tanvirhussein@gmail.com>
2021-10-20 06:39:46 -0400

    all tests passed

--- api/index.js
+++ api/index.js
@@ -1,53 +1,53 @@
-// Build an apiRouter using express Router
+const apiRouter = require("express").Router();
 
+const {
+  createReport,
+  closeReport,
+  getOpenReports,
+  createReportComment,
+} = require("../db");
 
-// Import the database adapter functions from the db
+apiRouter.get("/reports", async (req, res, next) => {
+  try {
+    const reports = await getOpenReports();
+    res.send({ reports });
+  } catch (error) {
+    next(error);
+  }
+});
 
+apiRouter.post("/reports", async (req, res, next) => {
+  try {
+    const newReport = await createReport(req.body);
 
-/**
- * Set up a GET request for /reports
- * 
- * - it should use an async function
- * - it should await a call to getOpenReports
- * - on success, it should send back an object like { reports: theReports }
- * - on caught error, call next(error)
- */
+    res.send(newReport);
+  } catch (error) {
+    next(error);
+  }
+});
 
+apiRouter.delete("/reports/:reportId", async (req, res, next) => {
+  try {
+    const { reportId } = req.params;
+    const { password } = req.body;
 
+    const result = await closeReport(reportId, password);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-/**
- * Set up a POST request for /reports
- * 
- * - it should use an async function
- * - it should await a call to createReport, passing in the fields from req.body
- * - on success, it should send back the object returned by createReport
- * - on caught error, call next(error)
- */
+apiRouter.post("/reports/:reportId/comments", async (req, res, next) => {
+  try {
+    const { reportId } = req.params;
 
+    const newComment = await createReportComment(reportId, req.body);
 
+    res.send(newComment);
+  } catch (error) {
+    next(error);
+  }
+});
 
-/**
- * Set up a DELETE request for /reports/:reportId
- * 
- * - it should use an async function
- * - it should await a call to closeReport, passing in the reportId from req.params
- *   and the password from req.body
- * - on success, it should send back the object returned by closeReport
- * - on caught error, call next(error)
- */
-
-
-
-/**
- * Set up a POST request for /reports/:reportId/comments
- * 
- * - it should use an async function
- * - it should await a call to createReportComment, passing in the reportId and
- *   the fields from req.body
- * - on success, it should send back the object returned by createReportComment
- * - on caught error, call next(error)
- */
-
-
-
-// Export the apiRouter
+module.exports = apiRouter;
--- db/index.js
+++ db/index.js
@@ -1,10 +1,8 @@
-// Require the Client constructor from the pg package
-const { query } = require("express");
 const { Client } = require("pg");
-require("dotenv").config();
-// Create a constant, CONNECTION_STRING, from either process.env.DATABASE_URL or postgres://localhost:5432/phenomena-dev
-const CONNECTION_STRING = process.env.CONNECTION_STRING;
-// Create the client using new Client(CONNECTION_STRING)
+
+const CONNECTION_STRING =
+  process.env.DATABASE_URL || "postgres://localhost:5432/phenomena-dev";
+
 const client = new Client({
   connectionString: CONNECTION_STRING,
   ssl:
@@ -12,188 +10,159 @@ const client = new Client({
       ? { rejectUnauthorized: false }
       : undefined,
 });
-// Do not connect to the client in this file!
 
-/**
- * Report Related Methods
- */
-
-/**
- * You should select all reports which are open.
- *
- * Additionally you should fetch all comments for these
- * reports, and add them to the report objects with a new field, comments.
- *
- * Lastly, remove the password field from every report before returning them all.
- */
-async function getOpenReports() {
+const getOpenReports = async () => {
   try {
-    // first load all of the reports which are open
-
     const { rows: reports } = await client.query(`
-    SELECT * FROM reports
-    WHERE "isOpen" = 'true';
-`);
-    // then load the comments only for those reports, using a
-    // WHERE "reportId" IN () clause
+      SELECT *
+      FROM reports 
+      WHERE reports."isOpen" = 'true';
+    `);
+
     const { rows: comments } = await client.query(`
-SELECT * FROM comments
-WHERE "reportId" IN (${reports.map((report) => report.id).join(", ")});
-`);
-    // then, build two new properties on each report:
-    // .comments for the comments which go with it
-    //    it should be an array, even if there are none
-    // .isExpired if the expiration date is before now
-    //    you can use Date.parse(report.expirationDate) < new Date()
-    // also, remove the password from all reports
-    reports.map((report) => {
+      SELECT * 
+      FROM comments
+      WHERE "reportId" IN (${reports.map((report) => report.id).join(", ")});
+    `);
+
+    reports.forEach((report) => {
       report.comments = comments.filter(
         (comment) => comment.reportId === report.id
       );
       report.isExpired = Date.parse(report.expirationDate) < new Date();
       delete report.password;
     });
-    // finally, return the reports
+
     return reports;
   } catch (error) {
     throw error;
   }
-}
+};
 
-/**
- * You should use the reportFields parameter (which is
- * an object with properties: title, location, description, password)
- * to insert a new row into the reports table.
- *
- * On success, you should return the new report object,
- * and on failure you should throw the error up the stack.
- *
- * Make sure to remove the password from the report object
- * before returning it.
- */
-async function createReport(reportFields) {
-  // Get all of the fields from the passed in object
-  const { title, location, description, password } = reportFields;
+const createReport = async ({ title, location, description, password }) => {
   try {
-    // insert the correct fields into the reports table
-    // remember to return the new row from the query
     const {
       rows: [report],
     } = await client.query(
       `
-      INSERT INTO reports (title, location, description, password)
+      INSERT INTO reports(title, location, description, password)
       VALUES ($1, $2, $3, $4)
       RETURNING *;
     `,
       [title, location, description, password]
     );
-    // remove the password from the returned row
+
     delete report.password;
-    // return the new report
+
     return report;
   } catch (error) {
     throw error;
   }
-}
+};
 
-/**
- * NOTE: This function is not for use in other files, so we use an _ to
- * remind us that it is only to be used internally.
- * (for our testing purposes, though, we WILL export it)
- *
- * It is used in both closeReport and createReportComment, below.
- *
- * This function should take a reportId, select the report whose
- * id matches that report id, and return it.
- *
- * This should return the password since it will not eventually
- * be returned by the API, but instead used to make choices in other
- * functions.
- */
-async function _getReport(reportId) {
+const _getReport = async (reportId) => {
   try {
-    // SELECT the report with id equal to reportId
-    const {rows:[report]}=await client.query(`
-    SELECT * FROM reports
-    WHERE id=$1;
-    `,[reportId])
-    // return the report
-    return report
+    const {
+      rows: [report],
+    } = await client.query(
+      `
+      SELECT * FROM reports
+      WHERE id=$1;
+    `,
+      [reportId]
+    );
+
+    return report;
   } catch (error) {
     throw error;
   }
-}
+};
 
-/**
- * You should update the report where the reportId
- * and password match, setting isOpen to false.
- *
- * If the report is updated this way, return an object
- * with a message of "Success".
- *
- * If nothing is updated this way, throw an error
- */
-async function closeReport(reportId, password) {
+const closeReport = async (reportId, password) => {
   try {
-    // First, actually grab the report with that id
-    const report = await _getReport(reportId)
-    // If it doesn't exist, throw an error with a useful message
-    if(!report) {
-      throw Error(`Report with id ${reportId} does not exist`)
+    const report = await _getReport(reportId);
+
+    if (!report) {
+      throw Error("Report does not exist with that id");
     }
-    // If the passwords don't match, throw an error
-    if(report.password !== password) {
-      throw Error(`Password does not match for this report`)
+
+    if (report.password !== password) {
+      throw Error("Password incorrect for this report, please try again");
     }
-    // If it has already been closed, throw an error with a useful message
-    if(!report.isOpen) {
-      throw Error(`This report is closed.`)
+
+    if (!report.isOpen) {
+      throw Error("This report has already been closed");
     }
-    // Finally, update the report if there are no failures, as above
-    // Return a message stating that the report has been closed
-    await client.query(`
-    UPDATE reports
-    SET "isOpen"='false'
-    WHERE id = $1;
-    `,[reportId])
+
+    await client.query(
+      `
+      UPDATE reports
+      SET "isOpen"='false'
+      WHERE id=$1;
+    `,
+      [reportId]
+    );
 
     return {
-      message: 'Report successfully closed!'
-    }
+      message: "Report successfully closed!",
+    };
   } catch (error) {
     throw error;
   }
-}
+};
 
-/**
- * Comment Related Methods
- */
-
-/**
- * If the report is not found, or is closed or expired, throw an error
- *
- * Otherwise, create a new comment with the correct
- * reportId, and update the expirationDate of the original
- * report to CURRENT_TIMESTAMP + interval '1 day'
- */
-async function createReportComment(reportId, commentFields) {
-  // read off the content from the commentFields
+const createReportComment = async (reportId, commentFields) => {
+  const { content } = commentFields;
 
   try {
-    // grab the report we are going to be commenting on
-    // if it wasn't found, throw an error saying so
-    // if it is not open, throw an error saying so
-    // if the current date is past the expiration, throw an error saying so
-    // you can use Date.parse(report.expirationDate) < new Date() to check
-    // all go: insert a comment
-    // then update the expiration date to a day from now
-    // finally, return the comment
+    const report = await _getReport(reportId);
+
+    if (!report) {
+      throw Error("That report does not exist, no comment has been made");
+    }
+
+    if (!report.isOpen) {
+      throw Error("That report has been closed, no comment has been made");
+    }
+
+    if (Date.parse(report.expirationDate) < new Date()) {
+      throw Error(
+        "The discussion time on this report has expired, no comment has been made"
+      );
+    }
+
+    const {
+      rows: [comment],
+    } = await client.query(
+      `
+      INSERT INTO comments("reportId", content)
+      VALUES ($1, $2)
+      RETURNING *;
+    `,
+      [reportId, content]
+    );
+
+    await client.query(
+      `
+      UPDATE reports
+      SET "expirationDate" = CURRENT_TIMESTAMP + interval '1 day'
+      WHERE id=$1
+      RETURNING *;
+    `,
+      [reportId]
+    );
+
+    return comment;
   } catch (error) {
     throw error;
   }
-}
+};
 
-// export the client and all database functions below
-module.exports={
-  client
-}
+module.exports = {
+  client,
+  _getReport,
+  createReport,
+  closeReport,
+  getOpenReports,
+  createReportComment,
+};
